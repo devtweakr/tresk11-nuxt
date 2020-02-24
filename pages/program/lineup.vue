25c5ba853a9766ba0ff6cf32eda78f03ab1cce3b
Jurij Podgoršek <podgorsek.jurij@gmail.com>
2020-02-24 03:57:26 +0100

    Dan > dogodek hierarhija (brez vmesnika lokacije)

--- pages/program/lineup.vue
+++ pages/program/lineup.vue
@@ -5,30 +5,21 @@
     <div v-for="dan in dnevi">
       <h2>{{ dan.cas }}</h2>
 
-      {{ $log(dan) }}
-      <div class="row">
-        <div v-for="lokacija in dan.lokacije" class="col-md-6">
-          <h3>{{ lokacija.ime }}</h3>
-
-          {{ $log(lokacija) }}
-
-          <b-row>
-            <b-col
-              key="band._jv.id"
-              v-for="band in lokacija.nastopi"
-              class="dogodek mb-5"
-              cols="6"
-              sm="4"
-              md="3"
-            >
-              <nuxt-link v-if="band._jv" :to="`/program/artist/${band._jv.id}`">
-                <img :src="band.field_slika | treskSlika">
-                <p>{{ band.title }}</p>
-              </nuxt-link>
-            </b-col>
-          </b-row>
-        </div>
-      </div>
+      <b-row>
+        <b-col
+          :key="band._jv.id"
+          v-for="band in dan.nastopi"
+          class="dogodek mb-5"
+          cols="6"
+          sm="4"
+          md="3"
+        >
+          <nuxt-link v-if="band._jv" :to="`/program/artist/${band._jv.id}`">
+            <img :src="band.field_slika | treskSlika">
+            <p>{{ band.title }}</p>
+          </nuxt-link>
+        </b-col>
+      </b-row>
     </div>
   </div>
 </template>
@@ -52,35 +43,23 @@ export default {
         if (!dnevi[cas]) {
           dnevi[cas] = {
             cas,
-            lokacije: []
-          }
-        }
-
-        const lokacija = bend.field_lokacija ? bend.field_lokacija.title : ''
-        if (!dnevi[cas].lokacije[lokacija]) {
-          dnevi[cas].lokacije[lokacija] = {
-            ime: lokacija,
-            id: 'ne', // lokacija._jv.id,
             nastopi: []
           }
         }
 
-        dnevi[cas].lokacije[lokacija].nastopi.push(bend)
+        dnevi[cas].nastopi.push(bend)
 
         return dnevi
       }, {})
 
-      return Object.values(hierarhija).map(dan => ({
-        ...dan,
-        lokacije: Object.values(dan.lokacije)
-      }))
+      return hierarhija
     }
   },
   fetch ({ store, params }) {
     const query = ({
       sort: 'field_cas_nastopa,field_lokacija.title',
       'filter[field_leto.name][value]': '2020',
-      include: 'field_lokacija,field_slika'
+      include: 'field_slika'
     })
 
     return store.dispatch('drupal/get', ['node/band', { params: query }])
