9f1f4dfbb7db2263f3abfd315f59393cbf13a08a
Jan Venier <jan.venier@gmail.com>
2020-02-26 18:38:00 +0100

    id page za pogovor

---
+++ pages/program/pogovor/_id.vue
@@ -0,0 +1,42 @@
+<template>
+  <div>
+    <PageTitle :pageTitle="dogodek.title" />
+    <b-row>
+      <b-col>
+        <p v-html="$options.filters.drupalLinks(dogodek.body.value)" v-if="dogodek.body" class="text-justify" />
+      </b-col>
+    </b-row>
+  </div>
+</template>
+
+<script>
+import PageTitle from '@/components/PageTitle'
+
+export default {
+  components: {
+    PageTitle
+  },
+  computed: {
+    dogodek () {
+      return Object.values(this.$store.getters['drupal/get']('node--koncert'))[0]
+    }
+  },
+  fetch ({ store, params }) {
+    const id = decodeURIComponent(params.id)
+    // Poskusi tudi drupalov path alias
+    const alias = `/dogodek/${id}`
+
+    const query = {
+      'filter[field_leto.name][value]': '2020',
+      'filter[field_path]': alias
+    }
+    return store.dispatch('drupal/get', [`node/koncert`, {
+      params: query
+    }])
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
