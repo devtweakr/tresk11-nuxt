2dab66331567ebe26fb2622ee7c7d7595524ab85
Jurij Podgoršek <podgorsek.jurij@gmail.com>
2020-02-27 18:14:26 +0100

    Kategoriziranje sponzorjev, izkljucitev neprimernih zalozb

--- pages/sponzorji.vue
+++ pages/sponzorji.vue
@@ -1,23 +1,27 @@
 <template>
   <div>
     <PageTitle :pageTitle="pageTitle" />
-    <b-row>
-      <b-col
-        :key="'sponzor'"
-        v-for="sponzor in sponzorji"
-        class="sponzor mb-4"
-        cols="6"
-        sm="4"
-        md="3"
-      >
-        <a v-if="sponzor.field_link[0]" :href="sponzor.field_link[0].uri" target="_blank">
-          <b-img :src="sponzor.field_slika | treskSlika" class="sponzor-logo mt-3" fluid center />
-          <h4 class="text-center mt-3">
-            {{ sponzor.title }}
-          </h4>
-        </a>
-      </b-col>
-    </b-row>
+
+    <div v-for="kategorija in kategorije">
+      <h3>{{ kategorija.name }}</h3>
+      <b-row>
+        <b-col
+          :key="'sponzor'"
+          v-for="sponzor in kategorija.sponzorji"
+          class="sponzor mb-4"
+          cols="6"
+          sm="4"
+          md="3"
+        >
+          <a v-if="sponzor.field_link[0]" :href="sponzor.field_link[0].uri" target="_blank">
+            <b-img :src="sponzor.field_slika | treskSlika" class="sponzor-logo mt-3" fluid center />
+            <h4 class="text-center mt-3">
+              {{ sponzor.title }}
+            </h4>
+          </a>
+        </b-col>
+      </b-row>
+    </div>
   </div>
 </template>
 
@@ -32,12 +36,31 @@ export default {
     return { pageTitle: 'SPONZORJI' }
   },
   computed: {
-    sponzorji () { return this.$store.getters['drupal/get']('node--sponzor') }
+    kategorije () {
+      const vsiSponzorji = Object.values(this.$store.getters['drupal/get']('node--sponzor'))
+
+      return vsiSponzorji.reduce((kat, sponzor) => {
+        const kategorija = sponzor.field_tip_podpornika.name
+
+        if (!kat[kategorija]) {
+          kat[kategorija] = {
+            name: kategorija,
+            sponzorji: []
+          }
+        }
+
+        kat[kategorija].sponzorji.push(sponzor)
+        console.log('push', kategorija)
+
+        return kat
+      }, {})
+    }
   },
   fetch ({ store, params }) {
     const query = {
       'filter[field_leto.name][value]': '2020',
-      include: 'field_slika'
+      include: 'field_slika,field_tip_podpornika',
+      sort: 'field_weight'
     }
 
     return store.dispatch('drupal/get', ['node/sponzor', { params: query }])
