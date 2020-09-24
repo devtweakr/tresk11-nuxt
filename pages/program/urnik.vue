f0f518c8beb5c2a51543f494d1d8eff0090d70ca
Jurij Podgoršek <jurij@kompot.si>
2020-09-24 05:17:00 +0200

    Nov postkoronski urnik page

--- pages/program/urnik.vue
+++ pages/program/urnik.vue
@@ -23,14 +23,18 @@ export default {
   computed: {
     page () {
       // Basic page urnik
-      const pageId = 'c5ac728f-e8f2-4b1b-8af1-8f67d8338709'
+      // const pageId = 'c5ac728f-e8f2-4b1b-8af1-8f67d8338709'
+      // Urnik #2
+      const pageId = '0ed3742c-3632-4c03-a198-d37d9bea2a9d'
 
       return this.$store.getters['drupal/get']('node--page')[pageId]
     }
   },
   fetch ({ store, params }) {
     // Basic page urnik
-    const pageId = 'c5ac728f-e8f2-4b1b-8af1-8f67d8338709'
+    // const pageId = 'c5ac728f-e8f2-4b1b-8af1-8f67d8338709'
+    // Urnik #2
+    const pageId = '0ed3742c-3632-4c03-a198-d37d9bea2a9d'
 
     const query = {}
     return store.dispatch('drupal/get', [`node/page/${pageId}`, {
